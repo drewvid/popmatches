adcbb2fb810d29f3f4f02a4534ebccbc3b8256e9
Andrew Ritz <andy.e.ritz@gmail.com>
2026-05-12 12:17:20 +0200

    refactor: update FSM state management and network traversal to support recursive string accumulation

--- laughing.py
+++ laughing.py
@@ -3,7 +3,9 @@ import random
 
 
 class FSM(object):
-    output = ''
+    """
+    Simple Finite State Machine for string generation using popmatches.
+    """
 
     def __init__(self, name, initial, final, arcs):
         self.name = name
@@ -11,28 +13,35 @@ class FSM(object):
         self.final = final
         self.arcs = arcs
         self.finished = False
+        self.output = ''
 
     def exit_from_fsm(self):
         raise Exception('exiting from FSM')
 
-    def traverse_network(self, start_node):
+    def traverse_network(self, start_node, current_str):
+        """Recursively traverse the network, accumulating the generated string."""
+        global to, symbol
         for arc in self.arcs:
             if matches(arc, [start_node, mexp('?to'), mexp('?symbol')]):
                 if to == self.final:
-                    self.output += symbol
+                    self.output = current_str + symbol
                     self.finished = True
                     self.exit_from_fsm()
                 elif symbol == '#' and random.random() < 0.8:
-                    self.traverse_network(to)
+                    self.traverse_network(to, current_str)
                 elif symbol != '#':
-                    self.output += symbol
-                    self.traverse_network(to)
+                    self.traverse_network(to, current_str + symbol)
 
     def traverse(self):
+        """Start the FSM traversal from the initial node."""
         try:
-            self.traverse_network(self.initial)
-        except:
-            pass
+            self.traverse_network(self.initial, '')
+        except Exception as e:
+            if str(e) == 'exiting from FSM':
+                # Normal termination signal
+                pass
+            else:
+                print(f"Error during FSM traversal: {e}")
         self.pr()
 
     def pr(self):
